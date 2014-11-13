472eb86c131e4b61f99077f817bc5464a180e065
mitchmox <mitchmox@me.com>
2014-11-13 17:17:23 -0600

    idk what this is exactly

--- library/Main.cs
+++ library/Main.cs
@@ -9,6 +9,9 @@ namespace library
 			Console.WriteLine ("Welcome to the D. Moxinuzzi Library");
 			int r = 0;
 
+			string name = UI.PromptLine ("\nPlease enter your name: \n - ").ToLower();
+			int id = UI.PromptInt("\nPlease enter your ID Number\n - ");
+
 			//anyone have a better way of doing this?
 			do
 			{
@@ -18,7 +21,7 @@ namespace library
 
 				if (command.Contains ("out"))
 				{	
-					Commands.CheckOut ();
+					Commands.CheckOut (name);
 					r++;
 				}
 				else if (command.Contains ("return") || command.Contains ("in"))
