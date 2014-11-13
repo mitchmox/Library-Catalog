e55789be372b55e0fc140ebd4926934943345ebc
mitchmox <mitchmox@me.com>
2014-11-13 12:22:34 -0600

    added a do-while loop and a count variable ‘r’ to handle if the request made is not a valid request

--- library/Main.cs
+++ library/Main.cs
@@ -7,15 +7,29 @@ namespace library
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Welcome to the D. Moxinuzzi Library");
-			string command = UI.PromptLine ("How may we assist you? \n").ToLower();
-			Console.WriteLine ();
+			int redo = 0;
 
-			if (command.Contains ("out"))
-				Commands.CheckOut ();
-			else if (command.Contains ("return") || command.Contains("in"))
-				Commands.Return ();
-			else
-				Console.WriteLine ("Request not recognized. You can \"Check Out\" materials OR \"Return\" materials.");
+			do
+			{
+				string command = UI.PromptLine ("\nHow may we assist you? \n -").ToLower();
+				Console.WriteLine ();
+
+
+				if (command.Contains ("out"))
+				{	
+					Commands.CheckOut ();
+					redo++;
+				}
+				else if (command.Contains ("return") || command.Contains ("in"))
+				{
+					Commands.Return ();
+					redo++;
+				}
+				else
+				{
+					Console.WriteLine ("Request not recognized. You can \"Check Out\" materials OR \"Return\" materials.");
+				}
+			} while(redo == 0);
 		}
 	}
 }
